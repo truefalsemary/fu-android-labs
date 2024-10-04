081a2ad242444e1f6cb7b2237e761cae53b71982
truefalsemary <truefalsemary@gmail.com>
2024-10-04 12:36:46 +0300

    feat: done 2nd lab

--- Laba2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Laba2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        TextView ageTextView = findViewById(R.id.ageTextView);
+        TextView specTextView = findViewById(R.id.specTextView);
+
+        addToTheEndTextViewValue(nameTextView, "Мишель");
+        addToTheEndTextViewValue(ageTextView, "13");
+        addToTheEndTextViewValue(specTextView, "Программист");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void addToTheEndTextViewValue(TextView textView, String endValue){
+        String oldValue = textView.getText().toString();
+        String newValue = oldValue + " " +endValue;
+        textView.setText(newValue);
+
     }
 }
